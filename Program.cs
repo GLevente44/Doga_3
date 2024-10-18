bfd073d4dadc2de73cf86232f6eaf52d23badbbe
Levente <gecsel@kkszki.hu>
2024-10-18 11:36:22 +0200

    Diakpontszam project is created

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga_3
+{
+    internal class Program
+    {
+        public static void Diakpontszam()
+        {
+            
+
+            
+        static void Main(string[] args)
+        {
+            Diakpontszam();
+        }
+    }
+}
